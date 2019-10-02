5d935b5ea2fdca78d8f08bb7d0c50ad08f7db2e3
IqbalNur <49528318+iqbalnur32@users.noreply.github.com>
2019-10-02 08:49:36 +0700

    Add files via upload

---
+++ node_cron/app.js
@@ -0,0 +1,35 @@
+const cron = require('node-cron');
+const nodemailer = require('nodemailer');
+
+// stack cron
+const task = cron.schedule('5 * * * *', () => {
+    sendMail().then(console.log('succes'));
+});
+
+// send email
+async function sendMail() {
+    let  configMail, transporter, emailTarget, mail;
+    
+    // Service email
+    configMail = {
+      service: 'gmail',
+      auth: {
+        user: 'your gmail',
+        pass: 'your password gmail'
+      }
+    };
+  
+    transporter = await nodemailer.createTransport(configMail);
+    emailTarget = 'your target email';
+    
+    // email send target
+    mail = {
+      to: emailTarget,
+      from: configMail.auth.user,
+      subject: '[Penting Bro ] Backup Pesan',
+      html: `Hallo Iqbal Nur Backup Messages Succes Please And Enjoyy <br> Thanks Mr`
+    };
+    transporter.sendMail(mail);
+}
+
+task.start();
